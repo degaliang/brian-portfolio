9c72b1ed5eb044fb3a354d9ff1a3e2532d2e1588
Alex Liang <degaliang@berkeley.edu>
2025-04-12 17:36:45 -0700

    resolve image path issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'brian-portfolio';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true, // disable server-side image optimization
+  }
+};
 
 export default nextConfig;
