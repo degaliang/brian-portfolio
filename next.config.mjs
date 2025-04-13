24d81c6a2e2a8cbab34bee79a960262d38e6633d
Alex Liang <degaliang@berkeley.edu>
2025-04-12 18:01:26 -0700

    Deploy static site to docs for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+    basePath: '',        // keep this empty for root deployment
+    assetPrefix: '',     // keep this empty for root deployment
+};
+  
 export default nextConfig;
+  
