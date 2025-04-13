4e9ef37af1e401922d2e529097847178369b8141
Alex Liang <degaliang@berkeley.edu>
2025-04-12 17:43:04 -0700

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = 'brian-portfolio';
-
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
-  images: {
-    unoptimized: true, // disable server-side image optimization
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
