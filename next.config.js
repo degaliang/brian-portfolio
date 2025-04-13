1fad0dac1565b30378f9e7abc93d08ad704ddf91
Alex Liang <degaliang@berkeley.edu>
2025-04-12 19:11:11 -0700

    try .js for config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     assetPrefix: '/brian-portfolio',
+//     basePath: '/brian-portfolio',
+//     images: {
+//         unoptimized: true,
+//     }
+// };
+
+// export default nextConfig;
+
+const nextConfig={
+    basePath: '/brian-portfolio'
+}
+
+module.exports = nextConfig;
